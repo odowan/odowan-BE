4c43f5d034f589bd48898a0ddd7375725ba8bd59
khw18 <gmsb14@naver.com>
2023-09-19 19:30:35 +0900

    first

---
+++ src/main/java/com/smu/odowan/OdowanApplication.java
@@ -0,0 +1,13 @@
+package com.smu.odowan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdowanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdowanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smu/odowan/OdowanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smu.odowan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdowanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
